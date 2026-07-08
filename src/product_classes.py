793c6af98bf4a797f3ffa015171a6004bca99093
Darya Melenteva <d.v.melenteva@gmail.com>
2026-07-08 23:12:09 +0300

    finish

--- src/product_classes.py
+++ src/product_classes.py
@@ -14,10 +14,42 @@ class Product:
         """Метод для инициализации экземпляра класса. Задаем значения атрибутам экземпляра."""
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
 
         Product.number_of_product += 1
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price):
+        if new_price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+        elif new_price < self.__price:
+            user_answer = input("Вы уверены, что хотите понизить цену? (y/n): ")
+            if user_answer.lower() == 'y':
+                self.__price = new_price
+            else:
+                print("Действие отменено")
+        else:
+            self.__price = new_price
+
+    @classmethod
+    def new_product(cls, product_data, current_products=None):
+        if current_products:
+            for existing_product in current_products:
+                if product_data['name'] == existing_product.name:
+                    existing_product.quantity += product_data['quantity']
+                    existing_product.price = max(existing_product.price, product_data['price'])
+                    return existing_product
+        return cls(
+            name=product_data['name'],
+            description=product_data['description'],
+            price=product_data['price'],
+            quantity=product_data['quantity']
+        )
 
 
 class Category:
@@ -26,7 +58,7 @@ class Category:
     # Аннотации типов в вашем стиле
     name: str
     description: str
-    products: list
+    __products: list
 
     # Переменные на уровне класса для подсчета категорий и уникальных товаров
     category_count = 0
@@ -36,10 +68,25 @@ class Category:
         """Метод для инициализации экземпляра класса. Задаем значения атрибутам экземпляра."""
         self.name = name
         self.description = description
-        self.products = products  # Сюда передаем список объектов класса Product
+        self.__products = products  # Сюда передаем список объектов класса Product
 
         # Автоматически увеличиваем количество категорий на 1
         Category.category_count += 1
 
         # Автоматически прибавляем количество переданных товаров к общему счетчику
         Category.product_count += len(products)
+
+    def add_product(self, product):
+        """Метод принимает объект класса Product и записывает его в приватный список товаров."""
+
+        self.__products.append(product)
+        Category.product_count += 1
+
+    @property
+    def products(self):
+        """Геттер возвращает информацию о товарах в виде строк заданного формата."""
+        result = ""
+        for prod in self.__products:
+            # Форматируем строку строго по вашему ТЗ
+            result += f"{prod.name}, {prod.price} руб. Остаток: {prod.quantity} шт.\n"
+        return result
